2ff8100adf7df82a7e674e39efeaf01f77148d69
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2023-08-15 00:40:28 +0530

    Formatting

--- 07dfs.cpp
+++ 07dfs.cpp
@@ -5,16 +5,12 @@ using namespace std::chrono;
 void DFS(int startVertex, int** graph, int numVertices) {
     bool* visited = new bool[numVertices] {false};
     stack<int> visitedStack;
-
     visitedStack.push(startVertex);
     visited[startVertex] = true;
-
     while (!visitedStack.empty()) {
         int vertex = visitedStack.top();
         visitedStack.pop();
-
         cout << vertex << " ";
-
         for (int i = 0; i < numVertices; ++i) {
             if (graph[vertex][i] && !visited[i]) {
                 visitedStack.push(i);
@@ -22,27 +18,21 @@ void DFS(int startVertex, int** graph, int numVertices) {
             }
         }
     }
-
-   
 }
 
 int main() {
     const int numVertices = 7;
     int** graph = new int*[numVertices];
-
     for (int i = 0; i < numVertices; ++i) {
         graph[i] = new int[numVertices] {0};
     }
-
     graph[0][1] = 1;
     graph[0][2] = 1;
     graph[1][3] = 1;
     graph[1][4] = 1;
     graph[2][5] = 1;
     graph[2][6] = 1;
-
     int startVertex = 0;
-
     cout << "DFS Traversal: ";
     auto start = high_resolution_clock::now();
     DFS(startVertex, graph, numVertices);
@@ -50,7 +40,5 @@ int main() {
     auto dur = duration_cast<nanoseconds>(end - start).count();
     cout << endl;
     cout << "The time taken for the traversal is " << dur <<" nanoseconds." << endl;
-
-
     return 0;
 }
