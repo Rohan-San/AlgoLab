5772cc7f1d88e85704a40bd8b193ac3d8127c7bd
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2023-08-15 00:41:35 +0530

    Formatting

--- 06bfs.cpp
+++ 06bfs.cpp
@@ -1,19 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 using namespace std::chrono;
+
 void BFS(int startVertex, int** graph, int numVertices) {
     bool* visited = new bool[numVertices] {false};
     queue<int> visitedQueue;
-
     visitedQueue.push(startVertex);
     visited[startVertex] = true;
-
     while (!visitedQueue.empty()) {
         int vertex = visitedQueue.front();
         visitedQueue.pop();
-
         cout << vertex << " ";
-
         for (int i = 0; i < numVertices; ++i) {
             if (graph[vertex][i] && !visited[i]) {
                 visitedQueue.push(i);
@@ -21,27 +18,21 @@ void BFS(int startVertex, int** graph, int numVertices) {
             }
         }
     }
-
-    
 }
 
 int main() {
     const int numVertices = 7;
     int** graph = new int*[numVertices];
-
     for (int i = 0; i < numVertices; ++i) {
         graph[i] = new int[numVertices] {0};
     }
-
     graph[0][1] = 1;
     graph[0][2] = 1;
     graph[1][3] = 1;
     graph[1][4] = 1;
     graph[2][5] = 1;
     graph[2][6] = 1;
-
     int startVertex = 0;
-
     cout << "BFS Traversal: ";
     auto start = high_resolution_clock::now();
     BFS(startVertex, graph, numVertices);
