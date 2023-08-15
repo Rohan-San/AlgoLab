bd4183c6e215c996fa6d0ba5eef619c9c2a7edb3
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2023-08-15 23:17:06 +0530

    Replaced with easier code

--- 07dfs.cpp
+++ 07dfs.cpp
@@ -1,44 +1,51 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<ctime>
 using namespace std;
-using namespace std::chrono;
+#define MAX 100
 
-void DFS(int startVertex, int** graph, int numVertices) {
-    bool* visited = new bool[numVertices] {false};
-    stack<int> visitedStack;
-    visitedStack.push(startVertex);
-    visited[startVertex] = true;
-    while (!visitedStack.empty()) {
-        int vertex = visitedStack.top();
-        visitedStack.pop();
-        cout << vertex << " ";
-        for (int i = 0; i < numVertices; ++i) {
-            if (graph[vertex][i] && !visited[i]) {
-                visitedStack.push(i);
-                visited[i] = true;
+bool visited[MAX];
+int adj[MAX][MAX];
+
+void dfs(int start, int n)
+{
+    int stack[MAX];
+    int top = -1;
+    int v;
+    visited[start] = true;
+    stack[++top] = start;
+    while(top!=-1)
+    {
+        v = stack[top--];
+        cout<<v<<" ";
+        for(int i=0;i<n;i++)
+        {
+            if(adj[v][i]&& !visited[i])
+            {
+                visited[i]=true;
+                stack[++top]=i;    
             }
         }
     }
+    cout<<endl;
 }
 
-int main() {
-    const int numVertices = 7;
-    int** graph = new int*[numVertices];
-    for (int i = 0; i < numVertices; ++i) {
-        graph[i] = new int[numVertices] {0};
-    }
-    graph[0][1] = 1;
-    graph[0][2] = 1;
-    graph[1][3] = 1;
-    graph[1][4] = 1;
-    graph[2][5] = 1;
-    graph[2][6] = 1;
-    int startVertex = 0;
-    cout << "DFS Traversal: ";
-    auto start = high_resolution_clock::now();
-    DFS(startVertex, graph, numVertices);
-    auto end = high_resolution_clock::now();
-    auto dur = duration_cast<nanoseconds>(end - start).count();
-    cout << endl;
-    cout << "The time taken for the traversal is " << dur <<" nanoseconds." << endl;
+int main()
+{
+    int start, n;
+    cout<<"enter num of vertices: ";
+    cin>>n;
+    cout<<"enter vertices:"<<endl;
+    for(int i = 0;i<n;i++)
+        for(int j=0;j<n;j++)
+            cin>>adj[i][j];
+    cout<<"enter starting vertice: ";
+    cin>>start;
+    for (int i=0;i<n;i++)
+        visited[i] = false;
+    clock_t start_t = clock();
+    dfs(start, n);
+    clock_t end = clock();
+    double time = double(end-start_t)/CLOCKS_PER_SEC;
+    cout<<"time taken: "<<time*1000000<<" Microsseconds\n";
     return 0;
 }
